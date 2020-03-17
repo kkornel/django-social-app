cb091e0c3af4dd2574bf40624b233f8dbbbcbd4d
kkornel <kornel.krzeslak@student.put.poznan.pl>
2020-03-17 04:48:57 +0100

    Before changing styles

--- social/models.py
+++ social/models.py
@@ -42,98 +42,6 @@ class Post(models.Model):
             img.thumbnail(output_size)
             img.save(self.image.path)
 
-    # def save(self, *args, **kwargs):
-    #     """ Resizing images on S3.
-    #     Calculates height based on desired width.
-    #     """
-    #     super().save(*args, **kwargs)
-
-    #     if not self.image:
-    #         return
-
-    #     desired_width = 600
-
-    #     img_read = storage.open(self.image.name, 'r')
-    #     img = Image.open(img_read)
-
-    #     if img.height > desired_width or img.width > desired_width:
-    #         extension = os.path.splitext(self.image.name)[1].lower()
-
-    #         if extension in ['.jpeg', '.jpg']:
-    #             format = 'JPEG'
-    #         if extension in ['.png']:
-    #             format = 'PNG'
-
-    #         wpercent = (desired_width / float(img.size[0]))
-    #         hsize = int((float(img.size[1]) * float(wpercent)))
-    #         img = img.resize((desired_width, hsize), Image.ANTIALIAS)
-    #         in_mem_file = io.BytesIO()
-    #         img.save(in_mem_file, format=format)
-    #         img_write = storage.open(self.image.name, 'w+')
-    #         img_write.write(in_mem_file.getvalue())
-    #         img_write.close()
-
-    #     img_read.close()
-
-    # def save(self, *args, **kwargs):
-    # """ Resizing images on S3.
-    # Hardcoded output_size.
-    # """
-    # super().save(*args, **kwargs)
-
-    # if not self.image:
-    #     return
-
-    # img_read = storage.open(self.image.name, 'r')
-    # img = Image.open(img_read)
-
-    # if img.height > 300 or img.width > 300:
-    #     extension = os.path.splitext(self.image.name)[1].lower()
-
-    #     if extension in ['.jpeg', '.jpg']:
-    #         format = 'JPEG'
-    #     if extension in ['.png']:
-    #         format = 'PNG'
-
-    #     output_size = (300, 300)
-    #     img.thumbnail(output_size)
-    #     in_mem_file = io.BytesIO()
-    #     img.save(in_mem_file, format=format)
-    #     img_write = storage.open(self.image.name, 'w+')
-    #     img_write.write(in_mem_file.getvalue())
-    #     img_write.close()
-
-    # img_read.close()
-
-    # def save(self, *args, **kwargs):
-    # """ Resizing images on S3.
-    # Using AWS example - divides size by 2.
-    # """
-    # super().save(*args, **kwargs)
-
-    # if not self.image:
-    #     return
-
-    # img_read = storage.open(self.image.name, 'r')
-    # img = Image.open(img_read)
-
-    # if img.height > 300 or img.width > 300:
-    #     extension = os.path.splitext(self.image.name)[1].lower()
-
-    #     if extension in ['.jpeg', '.jpg']:
-    #         format = 'JPEG'
-    #     if extension in ['.png']:
-    #         format = 'PNG'
-
-    #     img.thumbnail(tuple(x / 2 for x in img.size))
-    #     in_mem_file = io.BytesIO()
-    #     img.save(in_mem_file, format='JPEG')
-    #     img_write = storage.open(self.image.name, 'w+')
-    #     img_write.write(in_mem_file.getvalue())
-    #     img_write.close()
-
-    # img_read.close()
-
     def __str__(self):
         return f'Post#{self.id} by {self.author.user.username}#{self.author.user.id} -> {self.content[:10]}... '
 
@@ -150,8 +58,6 @@ class Comment(models.Model):
                                related_name='comments')
     text = models.TextField(max_length=280)
     date_commented = models.DateTimeField(default=timezone.now)
-
-    # date_commented = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'Comment#{self.id} for Post#{self.post.id} by {self.author.user.username}#{self.author.user.id}  -> Content {self.text[:10]}... '
